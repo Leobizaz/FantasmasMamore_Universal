12779f6440362df52c1f65ba8b73c617096f86c9
GuiSR388 <guicrilopez@users.noreply.github.com>
2019-05-15 17:22:04 -0300

    Porta do galpão fecha, inimigos aparecem.

---
+++ FantasmasMamoré_Main/Assets/Scripts/ControleTempoGalpao.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleTempoGalpao : MonoBehaviour
+{
+    public float Timeleft = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timeleft -= Time.deltaTime;
+    }
+}
---
+++ FantasmasMamoré_Main/Assets/Scripts/FechaGalpao.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FechaGalpao : MonoBehaviour
+{
+
+    public GameObject porta;
+    private AudioSource Au;
+    public AudioClip inpact;
+    public GameObject E;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Au = porta.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            porta.transform.localPosition = new Vector3(-123.93f, 53.245f, 47.62f);
+            E.SetActive(true);
+            Au.PlayOneShot(inpact, 0.4f);
+            Destroy(this.gameObject);
+                
+        }
+    }
+}
