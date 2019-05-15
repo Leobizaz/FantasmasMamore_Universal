7cea76ce6bb9da7d1d39284baf2f3c987045fe5a
Leobizaz <leobizaz12@gmail.com>
2019-05-14 23:32:01 -0300

    +Mecanica da mulher finalizada | +Dialogos implementados

--- FantasmasMamoré_Main/Assets/MulherBehaviour.cs
+++ FantasmasMamoré_Main/Assets/MulherBehaviour.cs
@@ -22,10 +22,23 @@ public class MulherBehaviour : MonoBehaviour
     {
         anim.Play("surprised");
         Invoke("NextPlace1", 3f);
-
     }
     void NextPlace1()
     {
         agent.SetDestination(new Vector3(-111, 50, 316));
+    }
+
+    public void Corre2()
+    {
+        agent.SetDestination(new Vector3(-120, 52, 229));
+    }
+
+    public void Corre3()
+    {
+        agent.SetDestination(new Vector3(-125, 52, 85));
+    }
+    public void Corre4()
+    {
+        agent.SetDestination(new Vector3(-124, 52, 41));
     }
 }
--- FantasmasMamoré_Main/Assets/MulherTrigger.cs
+++ FantasmasMamoré_Main/Assets/MulherTrigger.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class MulherTrigger : MonoBehaviour
 {
     public MulherBehaviour code;
+    public int ID;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            code.Corre1();
+            if (ID == 1)
+            {
+                code.Corre1();
+            }else if (ID == 2)
+            {
+                code.Corre2();
+            }else if(ID == 3)
+            {
+                code.Corre3();
+            }else if(ID == 4)
+            {
+                code.Corre4();
+            }
             Destroy(gameObject);
         }
     }
