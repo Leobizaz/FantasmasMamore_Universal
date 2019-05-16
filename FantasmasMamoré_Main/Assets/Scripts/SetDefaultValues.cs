91517d8015aa0314dc7470dd1c8bf4262e2b1f01
Leobizaz <leobizaz12@gmail.com>
2019-05-16 02:33:17 -0300

    +Intro dos headphones

--- FantasmasMamoré_Main/Assets/Scripts/SetDefaultValues.cs
+++ FantasmasMamoré_Main/Assets/Scripts/SetDefaultValues.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SetDefaultValues : MonoBehaviour
 {
+
+    public GameObject intro;
     void Start()
     {
         VolumeBrightness.Saved_brilho = 0.2f;
         VolumeBrightness.Saved_volumeMusica = 0.5f;
         VolumeBrightness.Saved_volumeVozes = 0.5f;
         VolumeBrightness.Saved_volumeEfeitos = 0.2f;
+        QualitySettings.SetQualityLevel(2);
+        Invoke("LoadIntro", 5f);
+        Invoke("LoadGame", 12f);
+    }
 
-        Invoke("LoadGame", 5f);
+    void LoadIntro()
+    {
+        intro.SetActive(true);
     }
 
     void LoadGame()
