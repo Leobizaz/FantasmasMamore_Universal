3166334a88153bc244ff490002e1973a4ae28d78
Leobizaz <leobizaz12@gmail.com>
2019-05-29 06:35:36 -0300

    +Creditos | +Jogo todo funcionando completo

---
+++ FantasmasMamoré_Main/Assets/CreditsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour
+{
+    public GameObject telaPause;
+    public Animator anim;
+    bool paused;
+
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Pause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+        if (Input.GetKey(KeyCode.Space) && !paused) anim.speed = 4f;
+        if (Input.GetKeyUp(KeyCode.Space) && !paused) anim.speed = 1f;
+    }
+
+    public void Pause()
+    {
+        paused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        telaPause.gameObject.SetActive(true);
+    }
+
+    public void UnPause()
+    {
+        paused = false;
+        Cursor.visible = true;
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        telaPause.gameObject.SetActive(false);
+    }
+}
---
+++ FantasmasMamoré_Main/Assets/ResetGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+    public void Reset()
+    {
+        Globals.playerProgress = 0;
+        Globals.photoscollected = 0;
+        SceneManager.LoadScene("MenuScene");
+    }
+}
