9d6fce5f655f247f927e5c121a1e42689ca51ecb
Leobizaz <leobizaz12@gmail.com>
2019-06-05 20:49:35 -0300

    Barreira no galpao agr suporta varias tabuas(3) falta a outra porta

--- FantasmasMamoré_Main/Assets/Scripts/BarreiraAtoFinal.cs
+++ FantasmasMamoré_Main/Assets/Scripts/BarreiraAtoFinal.cs
@@ -6,38 +6,43 @@ public class BarreiraAtoFinal : MonoBehaviour
 {
     public BoxCollider collider;
     [SerializeField] int health;
+    [SerializeField] int index = 0;
     public int maxHealth = 3;
     bool hitCooldown;
     public GameObject fx;
     public GameObject fxHit;
     public Vector3 droplocation;
-    public Vector3 tabuaLocation;
-    public Vector3 tabuaRotation;
-    GameObject StoredTabua;
+    public Vector3[] tabuaLocation = new Vector3[3];
+    public Vector3[] tabuaRotation = new Vector3[3];
+    GameObject[] StoredTabua = new GameObject[3];
     public bool stored;
 
     private void Update()
     {
         if (stored) collider.enabled = true;
         else collider.enabled = false;
+
+        if (index > 0) stored = true; else stored = false;
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "OBJ_Tabua" && !stored)
+        if (other.gameObject.name == "OBJ_Tabua" && index < 3)
         {
-            stored = true;
+            //stored = true;
             other.tag = "Untagged";
-            StoredTabua = other.gameObject;
+            StoredTabua[index] = other.gameObject;
             Rigidbody attachedRigidbody = other.GetComponent<Rigidbody>();
             other.transform.parent = this.transform;
-            other.transform.position = tabuaLocation;
-            other.transform.rotation = Quaternion.Euler(tabuaRotation);
+            other.transform.position = tabuaLocation[index];
+            other.transform.rotation = Quaternion.Euler(tabuaRotation[index]);
             other.attachedRigidbody.isKinematic = true;
             other.attachedRigidbody.useGravity = false;
             other.attachedRigidbody.constraints = RigidbodyConstraints.FreezeAll;
             this.gameObject.tag = "Interactable";
             health = maxHealth;
+            index++;
         }
 
         if (other.gameObject.tag == "Enemy" && stored)
@@ -51,14 +56,15 @@ public class BarreiraAtoFinal : MonoBehaviour
     {
         if(stored && Input.GetButtonDown("Fire1"))
         {
-            StoredTabua.tag = "Pickupable Object";
-            Rigidbody attachedRigidbody = StoredTabua.GetComponent<Rigidbody>();
-            StoredTabua.transform.parent = null;
+            StoredTabua[index-1].tag = "Pickupable Object";
+            Rigidbody attachedRigidbody = StoredTabua[index-1].GetComponent<Rigidbody>();
+            StoredTabua[index-1].transform.parent = null;
             attachedRigidbody.isKinematic = false;
             attachedRigidbody.useGravity = true;
             attachedRigidbody.constraints = RigidbodyConstraints.None;
-            StoredTabua.transform.position = droplocation;
-            stored = false;
+            StoredTabua[index-1].transform.position = droplocation;
+            index--;
+            //stored = false;
             this.gameObject.tag = "Untagged";
         }
     }
@@ -69,12 +75,12 @@ public class BarreiraAtoFinal : MonoBehaviour
         {
             if (!hitCooldown)
             {
-                HitaMadeira(StoredTabua);
+                HitaMadeira(StoredTabua[index]);
             }
 
             if (health == 0)
             {
-                QuebraMadeira(StoredTabua);
+                QuebraMadeira(StoredTabua[index]);
             }
         }
     }
@@ -101,7 +107,8 @@ public class BarreiraAtoFinal : MonoBehaviour
         //
         health = maxHealth;
         Destroy(tabua);
-        stored = false;
+        index--;
+        //stored = false;
     }
 
 }
